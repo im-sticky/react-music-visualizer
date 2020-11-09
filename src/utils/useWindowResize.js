6c41ea41fa3ef0e3ade86b5adb798e64e26e096d
Alex Craig <acraig780@gmail.com>
2020-11-09 14:01:00 -0500

    fix initial canvas size bug

--- src/utils/useWindowResize.js
+++ src/utils/useWindowResize.js
@@ -7,13 +7,15 @@ import debounce from 'lodash.debounce';
  * @param {number}   timing
  */
 export function useWindowResize(resizeEvent, timing = 10) {
-  const getWidth = () => window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  const getWidth = () => document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth;
 
   useEffect(() => {
     const resizeListener = debounce(() => {
       resizeEvent(getWidth());
     }, timing);
 
+    // call once for initial viewport size
+    resizeListener();
     window.addEventListener('resize', resizeListener);
 
     return () => window.removeEventListener('resize', resizeListener);
